bc6c06ef9179bb3756ea28870315d81728676bee
Adeons <adeons@gmail.com>
2014-07-16 15:14:24 -0300

    Apply PSR-2 format and comments to AttributeType code.

--- src/PhpEmber/AttributeType.php
+++ src/PhpEmber/AttributeType.php
@@ -1,14 +1,49 @@
 <?php
 namespace PhpEmber;
 
-final class AttributeType {
-	
-	const BOOLEAN_TYPE = 'boolean';
-	const INTEGER_TYPE = 'integer';
-	const FLOAT_TYPE = 'float';
-	const STRING_TYPE = 'string';
-	const DATE_TYPE = 'date';
-	const BELONGS_TO = 'belongsTo';
-	const HAS_MANY = 'hasMany';
-	
+/**
+ * Attribute type enumeration.
+ */
+final class AttributeType
+{
+
+    /**
+     * Boolean type.
+     */
+    const BOOLEAN_TYPE = 'boolean';
+
+    /**
+     * Integer number.
+     */
+    const INTEGER_TYPE = 'integer';
+
+    /**
+     * Number with decimals.
+     */
+    const FLOAT_TYPE = 'float';
+
+    /**
+     * String.
+     */
+    const STRING_TYPE = 'string';
+
+    /**
+     * Date and time.
+     * Valid values of this type are null or DateTime objects.
+     */
+    const DATE_TYPE = 'date';
+
+    /**
+     * Related object.
+     * Values of this type are either strings or null, where a string is the
+     * identifier of the related model, and null means that none is related.
+     */
+    const BELONGS_TO = 'belongsTo';
+
+    /**
+     * Related objects.
+     * The value of this type is always an array or a Traversable object.
+     */
+    const HAS_MANY = 'hasMany';
+
 }
